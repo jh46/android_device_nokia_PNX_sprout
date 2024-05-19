11edb467d94c50e1449ec006c40ac72bdb273066
Michael Bestas <mkbestas@lineageos.org>
2024-05-20 01:45:16 +0300

    PNX: Drop COMMON_LUNCH_CHOICES
    As of 14 QPR2 this is broken, let's remove it instead of updating it
    every 3 months. breakfast is the way to go.
    
    Change-Id: I097bd54b894d9a49c7ce8b1ab6f343349e268fd8

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,7 +1,2 @@
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/lineage_PNX_sprout.mk
-
-COMMON_LUNCH_CHOICES := \
-    lineage_PNX_sprout-user \
-    lineage_PNX_sprout-userdebug \
-    lineage_PNX_sprout-eng
