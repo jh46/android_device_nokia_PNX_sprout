a803e3d0caec880e9dfa8a9ad49b3f32b311de12
JH <39409110+jh46@users.noreply.github.com>
2024-05-23 22:53:20 +0300

    Revert "Phoenix: Use Pixel offline charger animation"
    This is not relevant for LineageOS.
    
    This reverts commit b82def4abc5bb62e38b2ea661ff0ef7a267d557b.

--- lineage_PNX_sprout.mk
+++ lineage_PNX_sprout.mk
@@ -6,7 +6,6 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 TARGET_BOOT_ANIMATION_RES := 1080
-TARGET_INCLUDE_PIXEL_CHARGER := true
 
 # Inherit from PNX_sprout device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
