d63ce28ee0d4289fe9db7e5c3ea0416d245b5906
Michael Bestas <mkbestas@lineageos.org>
2025-01-27 12:44:55 +0330

    PNX: Migrate mount point creation out of Android.mk
    Change-Id: Id5986b8a740e45f864eecdd2bd82d9455f128d6b

--- device.mk
+++ device.mk
@@ -279,6 +279,12 @@ PRODUCT_ENFORCE_RRO_TARGETS := *
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
     $(LOCAL_PATH)/overlay/packages/apps/CarrierConfig
 
+# Partitions
+PRODUCT_PACKAGES += \
+    vendor_bt_firmware_mountpoint \
+    vendor_dsp_mountpoint \
+    vendor_firmware_mnt_mountpoint
+
 # Perfd (dummy)
 PRODUCT_PACKAGES += \
     libqti-perfd-client
